14214b76a071c8f7ea07b687b5fc452405edc271
Jennifer Lipps <jjlipps@gmail.com>
2019-05-20 19:55:05 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,13 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 import 'bootstrap';
 import authString from './components/auth';
 import birfString from './components/birfday';
 
 const init = () => {
+  // console.error(apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   authString.authStringBuilder();
   birfString.birfStringBuilder();
 };
