5e6138196849ee14c5660625240151497d4a0b5a
Jennifer Lipps <jjlipps@gmail.com>
2019-05-20 21:27:06 -0500

    authentication working

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,10 +4,15 @@ import '../styles/main.scss';
 import 'bootstrap';
 import authString from './components/auth/auth';
 import birfString from './components/birfday';
+import myNavbar from './components/myNavbar/myNavbar';
+import authData from './helpers/data/authData';
+
 
 const init = () => {
   // console.error(apiKeys.firebaseKeys);
   firebase.initializeApp(apiKeys.firebaseKeys);
+  myNavbar.navbarEvents();
+  authData.checkLoginStatus();
   authString.authStringBuilder();
   birfString.birfStringBuilder();
 };
